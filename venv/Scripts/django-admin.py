44081b27b69ee0405abe63b1236e7987a88038b8
adhilroshan <adhilchra@gmail.com>
2023-04-09 16:13:52 +0530

    Initial Commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\downloads\classmanager-student-teacher-portal-master\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
